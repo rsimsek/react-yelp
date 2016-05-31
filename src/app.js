f79922a0553e2a481286459ccaa8436d5acafdfc
rsimsek <simsek.remzi@gmail.com>
2016-05-31 10:23:47 +0100

    First commit to React Yelp tutorial

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+    
+import styles from './styles.module.css'
+
+const App = React.createClass({
+  render: function() {
+    return (<div className = {styles['container']}>
+            Hello World!!!
+            </div>)
+  }
+});
+
+const mountNode = document.querySelector('#root');
+ReactDOM.render(<App />, mountNode);
